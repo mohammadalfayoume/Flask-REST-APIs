bba294e3ce77324c93eb06206653047f2cd84962
mohammadalfayoume <mabed3819@gmail.com>
2023-03-26 01:43:34 +0300

    first commit

---
+++ models/store.py
@@ -0,0 +1,10 @@
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__= "stores"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True,nullable=False)
+    items= db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
+    # cascade="all, delete" it means when you delete store, delete all items related to this store also
+    
