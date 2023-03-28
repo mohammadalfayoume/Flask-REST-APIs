1aa28215a18bca4d5385bddf98eae683a4a924fc
mohammadalfayoume <mabed3819@gmail.com>
2023-03-28 07:13:13 +0300

    JWT authentication

--- models/store.py
+++ models/store.py
@@ -7,4 +7,5 @@ class StoreModel(db.Model):
     name = db.Column(db.String(80), unique=True,nullable=False)
     items= db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
     # cascade="all, delete" it means when you delete store, delete all items related to this store also
+    # tags= db.relationship("TagModel", back_populates="store", lazy="dynamic")
     
--- schemas.py
+++ schemas.py
@@ -17,7 +17,22 @@ class ItemUpdateSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id= fields.Int(required=True, load_only=True)
     store= fields.Nested(PlainStoreSchema(), dump_only=True)
+
+# class PlainTagSchema(Schema):
+#     id = fields.Int(dump_only=True)
+#     name = fields.Str()
     
 class StoreSchema(PlainStoreSchema):
     items= fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
-    
+#     tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+# class TagSchema(PlainTagSchema):
+#     store_id = fields.Int(load_only=True)
+#     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username= fields.Str(required=True)
+    password= fields.Str(required=True, load_only=True)
+    # load_only=True it means that I don't want return the password in the response
+    # dump_only=True it means that the client doesn't have to insert the id because it is auto generated 
